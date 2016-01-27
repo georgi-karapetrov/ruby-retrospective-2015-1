ccb41dbeaf42170a0cb0259794780addb674f83a
Georgi Karapetrov <georgi.karapetrov1994@gmail.com>
2016-01-27 23:01:27 +0200

    Moved Hands classes to the respective Deck class

--- solutions/04.rb
+++ solutions/04.rb
@@ -19,6 +19,20 @@ class Card
 end
 
 class Deck
+  class Hand
+    def initialize(cards = [])
+      @cards = cards
+    end
+
+    def size
+      @cards.size
+    end
+
+    def add_card(card)
+      @cards << card
+    end
+  end
+
   include Enumerable
   SUITS = [:clubs, :diamonds, :hearts, :spades]
 
@@ -72,31 +86,17 @@ class Deck
   end
 end
 
-class Hand
-  def initialize(cards = [])
-    @cards = cards
-  end
-
-  def size
-    @cards.size
-  end
-
-  def add_card(card)
-    @cards << card
-  end
-end
-
-class WarHand < Hand
-  def play_card
-    @cards.pop
-  end
-
-  def allow_face_up?
-    @cards.size <= 3
-  end
-end
-
 class WarDeck < Deck
+  class WarHand < Deck::Hand
+    def play_card
+      @cards.pop
+    end
+
+    def allow_face_up?
+      @cards.size <= 3
+    end
+  end
+
   def deal
     hand = WarHand.new
     while hand.size < 26
@@ -106,8 +106,107 @@ class WarDeck < Deck
   end
 end
 
-
 class BeloteDeck < Deck
+  class BeloteHand < Deck::Hand
+    def highest_of_suit(suit)
+      deck_from_hand = BeloteDeck.new(@cards.select { |card| card.suit == suit })
+      deck_from_hand.sort
+      deck_from_hand.draw_top_card
+    end
+
+    def belote?
+      kings_and_queens = @cards.select { |card| king_or_queen(card) }
+      grouped_cards = kings_and_queens.group_by { |card| card.suit }
+      grouped_cards.any? { |_, value| value.size == 2 }
+    end
+
+    def next_card(card)
+      belote_deck_cards = BeloteDeck.new.to_a
+      found_card = belote_deck_cards.first
+
+      belote_deck_cards.each do |deck_card|
+
+        if deck_card == card
+          found_card = deck_card
+          break
+        end
+
+      end
+      card_index = belote_deck_cards.index(found_card)
+      belote_deck_cards[card_index + 1]
+    end
+
+    def tierce?
+      return false if @cards.size < 3
+      deck_from_hand = BeloteDeck.new(@cards)
+      deck_from_hand.sort
+      hand = deck_from_hand.to_a
+
+      hand.each_cons(3) do |first, second, third|
+        return true if consecutive_three?(first, second, third)
+      end
+
+      false
+    end
+
+    def quarte?
+      return false if @cards.size < 4
+      deck_from_hand = BeloteDeck.new(@cards)
+      deck_from_hand.sort
+      hand = deck_from_hand.to_a
+
+      hand.each_cons(4) do |first, second, third, fourth|
+        return true if consecutive_four?(first, second, third, fourth)
+      end
+
+      false
+    end
+
+    def quint?
+      return false if @cards.size < 5
+      deck_from_hand = BeloteDeck.new(@cards).sort
+      hand = deck_from_hand.to_a
+
+      hand.each_cons(5) do |first, second, third, fourth, fifth|
+        has_quart = consecutive_four?(first, second, third, fourth)
+        has_quint = has_quart and fourth == next_card(fifth)
+        return true if has_quint
+      end
+
+      false
+    end
+
+    def carre_of_jacks?
+      four_of_a_rank(:jack)
+    end
+
+    def carre_of_nines?
+      four_of_a_rank(9)
+    end
+
+    def carre_of_aces?
+      four_of_a_rank(:ace)
+    end
+
+    def consecutive_three?(first, second, third)
+        first == next_card(second) and second == next_card(third)
+    end
+
+    def consecutive_four?(first, second, third, fourth)
+      consecutive_three?(first, second, third) and third == next_card(fourth)
+    end
+
+    private
+
+    def four_of_a_rank(rank)
+      @cards.select { |card| card.rank == rank }.size == 4
+    end
+
+    def king_or_queen(card)
+      card.rank == :king or card.rank == :queen
+    end
+  end
+
   def ranks
     [7, 8, 9, :jack, :queen, :king, 10, :ace]
   end
@@ -122,108 +221,35 @@ class BeloteDeck < Deck
 end
 
 
-class BeloteHand < Hand
-
-  def highest_of_suit(suit)
-    deck_from_hand = BeloteDeck.new(@cards.select { |card| card.suit == suit })
-    deck_from_hand.sort
-    deck_from_hand.draw_top_card
-  end
-
-  def belote?
-    kings_and_queens = @cards.select { |card| king_or_queen(card) }
-    grouped_cards = kings_and_queens.group_by { |card| card.suit }
-    grouped_cards.any? { |_, value| value.size == 2 }
-  end
-
-  def next_card(card)
-    belote_deck_cards = BeloteDeck.new.to_a
-    found_card = belote_deck_cards.first
-
-    belote_deck_cards.each do |deck_card|
-
-      if deck_card == card
-        found_card = deck_card
-        break
-      end
-
-    end
-    card_index = belote_deck_cards.index(found_card)
-    belote_deck_cards[card_index + 1]
-  end
-
-  def tierce?
-    return false if @cards.size < 3
-    deck_from_hand = BeloteDeck.new(@cards)
-    deck_from_hand.sort
-    hand = deck_from_hand.to_a
-
-    hand.each_cons(3) do |first, second, third|
-      return true if consecutive_three?(first, second, third)
-    end
-
-    false
-  end
-
-  def quarte?
-    return false if @cards.size < 4
-    deck_from_hand = BeloteDeck.new(@cards)
-    deck_from_hand.sort
-    hand = deck_from_hand.to_a
-
-    hand.each_cons(4) do |first, second, third, fourth|
-      return true if consecutive_four?(first, second, third, fourth)
-    end
-
-    false
-  end
-
-  def quint?
-    return false if @cards.size < 5
-    deck_from_hand = BeloteDeck.new(@cards).sort
-    hand = deck_from_hand.to_a
-
-    hand.each_cons(5) do |first, second, third, fourth, fifth|
-      has_quart = consecutive_four?(first, second, third, fourth)
-      has_quint = has_quart and fourth == next_card(fifth)
-      return true if has_quint
-    end
-
-    false
-  end
-
-  def carre_of_jacks?
-    four_of_a_rank(:jack)
-  end
-
-  def carre_of_nines?
-    four_of_a_rank(9)
-  end
-
-  def carre_of_aces?
-    four_of_a_rank(:ace)
-  end
-
-  def consecutive_three?(first, second, third)
-      first == next_card(second) and second == next_card(third)
-  end
-
-  def consecutive_four?(first, second, third, fourth)
-    consecutive_three?(first, second, third) and third == next_card(fourth)
-  end
-
-  private
-
-  def four_of_a_rank(rank)
-    @cards.select { |card| card.rank == rank }.size == 4
-  end
-
-  def king_or_queen(card)
-    card.rank == :king or card.rank == :queen
-  end
-end
 
 class SixtySixDeck < Deck
+  class SixtySixHand < Deck::Hand
+    def twenty?(trump_suit)
+      kings_and_queens = @cards.select { |card| king_or_queen(card) }
+      groups = kings_and_queens.group_by { |card| card.suit }
+      pairs = groups.select { |key, value| twenty_pair?(key, value, trump_suit) }
+      pairs.size > 0
+    end
+
+    def forty?(trump_suit)
+      kings_and_queens = @cards.select { |card| king_or_queen(card)  }
+      groups = kings_and_queens.group_by { |card| card.suit }
+      pairs = groups.select { |key, value| forty_pair?(key, value, trump_suit) }
+      pairs.size > 0
+    end
+
+    def twenty_pair?(key, value, trump_suit)
+      key !=  trump_suit and value.size == 2
+    end
+
+    def forty_pair?(key, value, trump_suit)
+      key == trump_suit and value.size == 2
+    end
+
+    def king_or_queen(card)
+      card.rank == :king or card.rank == :queen
+    end
+  end
   def ranks
     [9, :jack, :queen, :king, 10, :ace]
   end
@@ -237,30 +263,3 @@ class SixtySixDeck < Deck
   end
 end
 
-class SixtySixHand < Hand
-  def twenty?(trump_suit)
-    kings_and_queens = @cards.select { |card| king_or_queen(card) }
-    groups = kings_and_queens.group_by { |card| card.suit }
-    pairs = groups.select { |key, value| twenty_pair?(key, value, trump_suit) }
-    pairs.size > 0
-  end
-
-  def forty?(trump_suit)
-    kings_and_queens = @cards.select { |card| king_or_queen(card)  }
-    groups = kings_and_queens.group_by { |card| card.suit }
-    pairs = groups.select { |key, value| forty_pair?(key, value, trump_suit) }
-    pairs.size > 0
-  end
-
-  def twenty_pair?(key, value, trump_suit)
-    key !=  trump_suit and value.size == 2
-  end
-
-  def forty_pair?(key, value, trump_suit)
-    key == trump_suit and value.size == 2
-  end
-
-  def king_or_queen(card)
-    card.rank == :king or card.rank == :queen
-  end
-end
