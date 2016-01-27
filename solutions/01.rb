96ad38f84a2d4e981d16b83febc2a2e2a9ef9d8f
Georgi Karapetrov <georgi.karapetrov1994@gmail.com>
2016-01-27 22:34:19 +0200

    Removed unnecessary method
    Added constant currency dictionary instead of a method.

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,16 +1,13 @@
-def get_currency_value(currency)
-  currencies = {usd: 1.7408, eur: 1.9557, gbp: 2.6415, bgn: 1.0}
-  currencies[currency]
-end
+CURRENCIES = {usd: 1.7408, eur: 1.9557, gbp: 2.6415, bgn: 1.0}
 
-def convert_to_bgn(value, currency)
-  result = value * get_currency_value(currency)
+def convert_to_bgn(price, currency)
+  result = price * CURRENCIES[currency]
   result.round(2)
 end
 
 def compare_prices(left_value, left_currency, right_value, right_currency)
-  left_result = left_value * get_currency_value(left_currency)
-  right_result = right_value * get_currency_value(right_currency)
+  left_result = left_value * CURRENCIES[left_currency]
+  right_result = right_value * CURRENCIES[right_currency]
   left_result <=> right_result
 end
 
